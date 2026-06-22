dda0fc99565e97a871b17b092d41b9d7ff08499b
aahsnr-configs <ahsanur041@proton.me>
2026-06-23 02:15:06 +0600

    updated

--- nvchad/lua/plugins/init.lua
+++ nvchad/lua/plugins/init.lua
@@ -44,7 +44,7 @@ return {
   --   config = true,
   -- },
   {
-    "MeanderinuugProgrammer/render-markdown.nvim",
+    "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
